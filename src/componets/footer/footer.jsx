51f8421ebb8ab51cad2de13401179d89e3b905cd
Билол <126669638+bilol-anvarov@users.noreply.github.com>
2023-12-03 15:02:19 +0500

    version 1.0

--- src/componets/footer/footer.jsx
+++ src/componets/footer/footer.jsx
@@ -8,7 +8,7 @@ const SectionFooter = () => {
                 <div className="links">
                     <a href="https://t.me/coool_kids" target="_blank">telegram</a>
                     <a href="https://www.instagram.com/coolkids5138.5139/" target="_blank">instagram</a>
-                    <a href="cool.kids.info.5138@gmail.com" target="_blank">mail</a>
+                    <a href="mailto:cool.kids.info.5138@gmail.com" target="_blank">mail</a>
                     <a href="tel:+998971566656" target="_blank">tel</a>
                 </div> 
                 <p className="copyright">copyright Cool Kids, all right reserved.<a href="https://github.com/bilol-anvarov/" target="_blank">© Bilol Developer</a> </p>
